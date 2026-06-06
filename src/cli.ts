650b4a44412c5cb4de602ee143a0fd66457f5c98
speedydelete <speedykorok777@gmail.com>
2026-06-06 13:43:15 -0400

    oops 764

--- src/cli.ts
+++ src/cli.ts
@@ -13,7 +13,7 @@ if (process.argv[2] === 'randomsearch') {
 //     process.exit(0);
 } else if (process.argv[2] === 'prove') {
     let lls = process.argv[3];
-    let {dx, dy, period} = parseSpeed(process.argv.slice(4).join(' '));
+    let {dx, dy, period} = parseSpeed(process.argv[4]);
     let maxBB = period * 2 + 4;
     let args = `-r 'pB1-c2345678/S012345678' -c -b ${maxBB} ${maxBB} -s p${period} x${dx} y${dy} -p '<4'`;
     console.log(`./lls ${args}`);
